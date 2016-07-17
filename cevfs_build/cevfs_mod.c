60778a409335d85461ba3b25f077a2e231edc52f
Ryan Homer <ryan@murage.ca>
2016-07-17 12:54:15 -0700

    Avoid use of sqlite3HexToBlob.

--- cevfs_build/cevfs_mod.c
+++ cevfs_build/cevfs_mod.c
@@ -1,6 +1,6 @@
 /**
 CEVFS - Compression & Encryption VFS
-cevfs_mod (shell module activation)
+cevfs_mod (module activation)
 
 Copyright (c) 2016 Ryan Homer, Murage Inc.
 
@@ -29,8 +29,58 @@ SOFTWARE.
 
 struct context ctx;
 
+/*
+** Taken from SQLite source code.
+** Check to see if this machine uses EBCDIC.  (Yes, believe it or
+** not, there are still machines out there that use EBCDIC.)
+*/
+#if 'A' == '\301'
+# define SQLITE_EBCDIC 1
+#else
+# define SQLITE_ASCII 1
+#endif
+
+/*
+** Taken from SQLite source code.
+** Translate a single byte of Hex into an integer.
+** This routine only works if h really is a valid hexadecimal
+** character:  0..9a..fA..F
+*/
+static u8 hexToInt(int h){
+  assert( (h>='0' && h<='9') ||  (h>='a' && h<='f') ||  (h>='A' && h<='F') );
+#ifdef SQLITE_ASCII
+  h += 9*(1&(h>>6));
+#endif
+#ifdef SQLITE_EBCDIC
+  h += 9*(1&~(h>>4));
+#endif
+  return (u8)(h & 0xf);
+}
+
+/*
+** Taken from SQLite source code.
+** Convert a BLOB literal of the form "x'hhhhhh'" into its binary
+** value.  Return a pointer to its binary value.  Space to hold the
+** binary value has been obtained from malloc and must be freed by
+** the calling routine.
+*/
+static void *hexToBlob(const char *z, int n){
+  char *zBlob;
+  int i;
+
+  zBlob = (char *)malloc(n/2 + 1);
+  n--;
+  if( zBlob ){
+    for(i=0; i<n; i+=2){
+      zBlob[i/2] = (hexToInt(z[i])<<4) | hexToInt(z[i+1]);
+    }
+    zBlob[i/2] = 0;
+  }
+  return zBlob;
+}
+
 void sqlite3_activate_cerod(const char *key) {
-  char *pKeyBytes = sqlite3HexToBlob(NULL, key+2, strlen(key+2)-1);
+  char *pKeyBytes = hexToBlob(key+2, strlen(key+2)-1);
 
   ctx.pKey   = pKeyBytes;          // 32-bit encryption hex key
   ctx.nKeySz = kCCKeySizeAES256;   // key size in bytes
